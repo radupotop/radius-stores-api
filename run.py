583c299dfd7ef8a7a27b5b4e91978f65e7ab92c9
Radu Potop <radu@wooptoo.com>
2019-03-11 12:25:06 +0000

    Added nearby results with KDtrees

--- run.py
+++ run.py
@@ -5,13 +5,18 @@ from pathlib import Path
 from flask import Flask, abort, jsonify
 from webargs import fields
 from webargs.flaskparser import use_args
+from scipy import spatial
 
 from model import Postcodes, db
 from schema import PostcodeSchema
 
 app = Flask(__name__)
 
-index_args = {'postcode': fields.Str(), 'nearby': fields.Str()}
+index_args = {
+    'postcode': fields.Str(),
+    'nearby': fields.Str(),
+    'radius': fields.Float(missing=0.5),
+}
 
 
 @app.route('/')
@@ -21,11 +26,22 @@ def index(args):
         query_result = Postcodes.select().where(
             Postcodes.postcode.contains(args['postcode'])
         )
-    elif args.get('nearby'):
-        result = Postcodes.select().where(Postcodes.postcode == args['nearby'])
-        lat = round(result[0].latitude, 2)
-        lng = round(result[0].longitude, 2)
-        print(lat, lng)
+    elif args.get('nearby') and args.get('radius'):
+        all_coordinates = Postcodes.select().where(
+            Postcodes.longitude, Postcodes.latitude
+        )
+        target = Postcodes.select().where(Postcodes.postcode == args['nearby'])[0]
+
+        coordinates_idx = tuple((c.longitude, c.latitude) for c in all_coordinates)
+        tree = spatial.KDTree(coordinates_idx)
+        nearby_idx = tree.query_ball_point(
+            (target.longitude, target.latitude), args['radius']
+        )
+
+        query_result = []
+        for idx in nearby_idx:
+            query_result.append(all_coordinates[idx])
+
     else:
         query_result = Postcodes.select().order_by(Postcodes.name)
 
